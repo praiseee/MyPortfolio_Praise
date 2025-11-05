43d40740985d89038cc6665e237c964df934ca95
praiseee <praisehoo07@gmail.com>
2025-11-05 15:51:34 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Spline from '@splinetool/react-spline';
+
+function App() {
+  return (
+  <Spline scene="https://prod.spline.design/MCqyRCSm5hP4NmQB/scene.splinecode" />
+  );
+}
+
+export default App
